78e25c82500ce963457d75f70f5f5dfb0cc938c9
Nicole Fejfar <nikki.fejfar@gmail.com>
2020-07-04 09:20:30 -0500

    Updated PyPoll Code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,44 +3,42 @@ import csv
 py_poll_csv = os.path.join("Resources", "election_data.csv")
 
 # Create variables.
-count = 0
-candidate_list = []
-unique_candidate_list = []
-vote_count_list = []
-vote_percent_list = []
+vote_count = {}
+total_votes = 0
+max_vote_count = 0
+winner = ""
 
-# Open file and create header.
+# Open file and skip header.
 with open(py_poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csvfile)
-    print("\nElection Results\n","-" *50, sep="")
 
-# Loop through each row to find and calculate values.
+    # Loop through each row to find candidate and vote count per candidate.
     for row in csvreader:
-        count += 1
-        candidate_list.append(row[2])
-# Loop through unique candidate list to calculate values.
-    for candidate in set(candidate_list):
-        unique_candidate_list.append(candidate)
-        vote_count = candidate_list.count(candidate)
-        vote_count_list.append(vote_count)
-        vote_percent = round((vote_count / count) *100, 3)
-        vote_percent_list.append(vote_percent)
-# Determine winner.
-    winning_vote_count = max(vote_count_list)
-    winner = unique_candidate_list[vote_count_list.index(winning_vote_count)]
+        candidate_name = row[2]
+        total_votes += 1
+        if candidate_name not in vote_count:
+            vote_count[candidate_name] = 1
+        else:
+            vote_count[candidate_name] += 1
 
-# Print results to terminal.
-print('Total Votes: ', str(count), "\n","-" * 50, sep="")
-for candidate in range(len(unique_candidate_list)):
-            print(unique_candidate_list[candidate] + ": " + str(vote_percent_list[candidate]) +"% (" + str(vote_count_list[candidate])+ ")")
-print("-" * 50, "\n", "The winner is: ", winner, "\n","-" * 50, sep="")
-
-# Create and write results to txt file.
+# Create text file to write print statements to.
 output_file = os.path.join("analysis", 'py_poll_analysis.txt')
 with open(output_file, "w") as txtFile:
+
+    # Print header and total vote counts to terminal and txt file.
+    print("Election Results\n","-" *50, sep="")
     print("Election Results\n","-" *50, sep="", file = txtFile)
-    print('Total Votes: ', str(count), "\n","-" * 50, sep="", file = txtFile)
-    for candidate in range(len(unique_candidate_list)):
-            print(unique_candidate_list[candidate] + ": " + str(vote_percent_list[candidate]) +"% (" + str(vote_count_list[candidate])+ ")", file = txtFile)
-    print("-" * 50, "\n", "The winner is: ", winner, "\n","-" * 50, sep="",  file = txtFile)
+    print('Total Votes: ', str(total_votes), "\n","-" * 50, sep="")
+    print('Total Votes: ', str(total_votes), "\n","-" * 50, sep="", file = txtFile)
+    
+    # Calculate candidate counts, percentages and winner, print to terminal and txt file.
+    for candidate_name, vote_count in vote_count.items():
+        vote_percent = round((vote_count / total_votes * 100), 3)
+        print(f'{candidate_name}: {vote_percent}% ({vote_count})')
+        print(f'{candidate_name}: {vote_percent}% ({vote_count})', file = txtFile)
+        if vote_count > max_vote_count:
+            winner = candidate_name
+            max_vote_count = vote_count
+    print("-" * 50, "\n", "The winner is: ", winner, "\n","-" * 50, sep="")
+    print("-" * 50, "\n", "The winner is: ", winner, "\n","-" * 50, sep="", file = txtFile)
